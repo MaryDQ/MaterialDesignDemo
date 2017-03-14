efff4128a1e7bda03723b7b06da8fef318120c26
MarkChaifer <964573692@qq.com>
2017-03-14 09:35:03 +0800

    创建Application类，初始化各种变量

---
+++ app/src/main/java/com/mlxphone/materialdesigndemo/InstaMaterialApplication.java
@@ -0,0 +1,15 @@
+package com.mlxphone.materialdesigndemo;
+
+import android.app.Application;
+
+/**
+ * Created by MLXPHONE on 2017/3/14.
+ */
+
+public class InstaMaterialApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
