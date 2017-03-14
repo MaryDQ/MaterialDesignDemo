9ca83def0edb9036fc205e706e07e35317366abd
MarkChaifer <964573692@qq.com>
2017-03-14 09:28:55 +0800

    编写Utils工具类，用来获取屏幕大小，判断手机系统版本是否大于LOLLIPOP

---
+++ app/src/main/java/com/mlxphone/materialdesigndemo/Utils.java
@@ -0,0 +1,52 @@
+package com.mlxphone.materialdesigndemo;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Point;
+import android.os.Build;
+import android.view.Display;
+import android.view.WindowManager;
+
+/**
+ * Created by MLXPHONE on 2017/3/14.
+ */
+
+public class Utils {
+    private static int screenWidth = 0;
+    private static int screenHeight = 0;
+
+    //dp转化为px像素
+    public static int dpToPx(int dp) {
+        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
+    }
+
+    //获取屏幕的高度
+    public static int getScreenHeight(Context context) {
+        if (screenHeight == 0) {
+            WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+            Display display = wm.getDefaultDisplay();
+            Point size = new Point();
+            display.getSize(size);
+            screenHeight = size.y;
+        }
+
+        return screenHeight;
+    }
+
+    //获取屏幕宽度
+    public static int getScreenWidth(Context c) {
+        if (screenWidth == 0) {
+            WindowManager wm = (WindowManager) c.getSystemService(Context.WINDOW_SERVICE);
+            Display display = wm.getDefaultDisplay();
+            Point size = new Point();
+            display.getSize(size);
+            screenWidth = size.x;
+        }
+
+        return screenWidth;
+    }
+
+    public static boolean isAndroid5() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
+    }
+}
